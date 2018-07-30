0b4c975d1e62d784c78d75de3021ab659458c3f2
SuperFamousDanV <djudev777@live.com>
2018-07-30 13:28:51 -0400

    routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,6 @@
 var db = require("../models");
 
 module.exports = function(app) {
-
   // Create a new example
   app.post("/api/examples", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
