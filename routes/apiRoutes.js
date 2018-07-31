107bb19348dc00ec7a03415ab11916dd80914084
SuperFamousDanV <djudev777@live.com>
2018-07-31 11:09:53 -0400

    _

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,12 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-  app.post("/api/users", function(req, res) {
-    db.User.create(req.body).then(function(dbUser) {
-      res.json(dbUser);
+  // Create a new example
+  app.post("/", function(req, res) {
+    db.Example.create(req.body).then(function(dbExample) {
+      res.json(dbExample);
     });
   });
+
+
 };
