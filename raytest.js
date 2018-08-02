1c55a8c974a4817e69c9dda0905b0a043309c10e
Ray <rkilburg42@gmail.com>
2018-08-02 14:34:14 -0400

    moment

--- raytest.js
+++ raytest.js
@@ -1,6 +1,17 @@
 var moment = require('moment');
-var startTime;
-var endTime;
-
-
+// var now = moment();
 moment().format();
+
+var now  = "04/09/2013 15:00:00";
+var then = "04/09/2013 14:20:30";
+var diff ;
+
+diff = moment.utc(moment(now,"DD/MM/YYYY HH:mm:ss").diff(moment(then,"DD/MM/YYYY HH:mm:ss"))).format("HH:mm:ss")
+
+console.log(diff);
+
+// if (now > date) {
+//    // date is past
+// } else {
+//    // date is future
+// }
