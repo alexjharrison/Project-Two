7e1e73d010ce429ad3c7faf90ee3ae25a67c29ac
Ray <rkilburg42@gmail.com>
2018-08-02 10:01:54 -0400

    post

--- public/js/index.js
+++ public/js/index.js
@@ -13,35 +13,38 @@ poster https://stackoverflow.com/questions/253689/switching-a-div-background-ima
 RT url  https://stackoverflow.com/questions/179713/how-to-change-the-href-for-a-hyperlink-using-jquery
 Route finder
 */
-var latitude, longitude;
 
 
-$(() => {
-    navigator.geolocation.getCurrentPosition(position=>{
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-    })
-    setTimeout(post,1000);
-    $("#submit").click(function (event) {
-        event.preventDefault();
-        post();
-    })
+var title = "space+jam";
+var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
+
+$.ajax({
+  url: queryURL,
+  method: "POST",
+  data: data
+}).then(function(response){
+  var results = response.data;
 })
 
-function post() {
-    console.log(latitude,longitude);
-    $.post("/", {
-        location: "123 Fake Street, Willingboro, NJ",
-        minRT: 60, //minimum percent score from rotten tomatoes
-        maxWait: 60,  //maximum wait in minutes from time to arrive at theater to movie start
-        disInclude: [], // array of genres not to include
-        maxRating: "R",  //not to include this rating or above
-        maxDistance: 10,  //max distance in miles to theater
-        latitude: latitude,
-        longitude: longitude
-    }, function (data, status) {
-        console.log(data, status);
-    });
+$("button").click(function(){
+  $.post("/",  {
 
-}
-
+    name: "best movie name",
+    rating: "R",
+    director: "estefan spielbergo",
+    stars: ["bobby bob","mary jean"],
+    genre: ["action, adventure"],
+    rtRating: "86",
+    metaCritic: "70",
+    posterImage: "www.posters.com/bestMovie.png",
+    description: "Some people do things",
+    showtime: "8:30pm",
+    theater: "amc multiplex",
+    time2Theater: 34,  //minutes to drive to theater
+    miles2Theater: 5,  //miles to drive to theater
+    gMapsLink: "http://maps.google.com/??"
+  },
+  function(data, status){
+    console.log(data, status);
+  });
+})
